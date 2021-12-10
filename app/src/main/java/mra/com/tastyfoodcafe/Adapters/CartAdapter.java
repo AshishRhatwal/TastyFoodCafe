60e2b5ef9d5699d965e251ae9a70fcd2ee7b790f
Ashish <49113101+AshishRhatwal@users.noreply.github.com>
2021-12-10 14:25:22 +0530

    Update CartAdapter.java

--- app/src/main/java/mra/com/tastyfoodcafe/Adapters/CartAdapter.java
+++ app/src/main/java/mra/com/tastyfoodcafe/Adapters/CartAdapter.java
@@ -47,8 +47,6 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.ViewHolder>
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
     {
         View view= LayoutInflater.from(mcontext).inflate(R.layout.addedcartcard,parent,false);
-
-
         return new CartAdapter.ViewHolder(view);
     }
 
@@ -58,44 +56,31 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.ViewHolder>
         final Bakeryitemss b=items.get(position);
         holder.prductname.setText(b.getPname());
         holder.prise.setText(b.getPrise());
-
-
-
+        
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(mcontext,"Proceed to Buy Now",Toast.LENGTH_SHORT).show();
-
             }
         });
-
-
-
-
-
-
     }
 
     @Override
     public int getItemCount() {
-
         return items.size();
     }
-
+    
     public class ViewHolder extends RecyclerView.ViewHolder {
-
+    
         TextView prductname,prise;
         ImageButton delete;
         Button buy;
-
+        
         public ViewHolder(View itemView) {
             super(itemView);
-
+            
             prductname = itemView.findViewById(R.id.im);
             prise=itemView.findViewById(R.id.text2);
-
-
-
 
         }
     }
